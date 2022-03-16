e9e8d90b2e994d85b1e9d52adf4eab96d1f0c93c
ChrisDev123 <christian.lima999@gmail.com>
2022-03-16 09:05:56 -0300

    Give up Pagination

--- src/Components/Signos listing/listing_signos.js
+++ src/Components/Signos listing/listing_signos.js
@@ -1,49 +1,50 @@
 import { useState } from "react";
 import { Col, Row } from "reactstrap";
 import CardsSignos from "../Cards/Cards_signos";
-import PaginationBtn from "../Pagination btns/pagination";
 import aries_img from '../../assets/imgs/aries.jpg';
 import touro_img from '../../assets/imgs/touro.jpg';
 import gemeos_img from '../../assets/imgs/gemeos.jpg';
+import aquario_img from "../../assets/imgs/aquario.jpg";
+import cancer_img from "../../assets/imgs/cancer.jpg";
+import capricornio_img from "../../assets/imgs/capricornio.jpg";
+import escorpiao_img from "../../assets/imgs/escorpiao.jpg";
+import leao_img from "../../assets/imgs/leao.jpg";
+import libra_img from "../../assets/imgs/libra.jpg";
+import peixes_img from "../../assets/imgs/peixes.jpg";
+import sargitario_img from "../../assets/imgs/sargitario.jpg";
+import virgem_img from "../../assets/imgs/virgem.jpg";
+
 
 export default function Listing(){
     const [list, setList] = useState([
-        {idSigno:'1', image:aries_img, nameSigno:"", description:"Teste"},
-        {idSigno:'2', image:touro_img, nameSigno:"", description:"Teste"},
-        {idSigno:'3', image:gemeos_img, nameSigno:"", description:"Teste"},
+        {image:aries_img, nameSigno:"Áries", description:"Teste"},
+        {image:touro_img, nameSigno:"Touro", description:"Teste"},
+        {image:gemeos_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:aquario_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:cancer_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:capricornio_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:escorpiao_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:leao_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:libra_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:peixes_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:sargitario_img, nameSigno:"Gemeos", description:"Teste"},
+        {image:virgem_img, nameSigno:"Gemeos", description:"Teste"},
     ]);
-    const [currentPage, setCurrentPage] = useState(0);
-
-    
 
     return(
         <>
             <section className='listing_box'>
-                <Row className='box_aligner'>
-                    <Col sm='1'>
-                        <div className='pagination previous_btn'>
-                            <PaginationBtn direction='left' modifyList={setList} currentList={list} changePage={setCurrentPage} verifierPage={currentPage} />
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className='cards_box'>
-                            <Row lg='3' md='2' sm='1'>
-                                    {list.map((signo)=>{
-                                        return(
-                                            <Col key={signo.idSigno} className='cards_delimiter'>
-                                                <CardsSignos image={signo.image} title={signo.nameSigno} subtitle='Teste' description={signo.description}/>
-                                            </Col>
-                                        );
-                                    })}    
-                            </Row>
-                        </div>
-                    </Col>
-                    <Col sm='1'>
-                        <div className='pagination next_btn'>
-                            <PaginationBtn direction='right' modifyList={setList} currentList={list} changePage={setCurrentPage} verifierPage={currentPage}/>
-                        </div>
-                    </Col>
-                </Row>
+                <div className='cards_box'>
+                    <Row lg='3' md='2' sm='1'>
+                            {list.map((signo)=>{
+                                return(
+                                    <Col key={signo.idSigno} className='cards_delimiter'>
+                                        <CardsSignos image={signo.image} title={signo.nameSigno} description={signo.description}/>
+                                    </Col>
+                                );
+                            })}    
+                    </Row>
+                </div>
             </section>
             <style jsx>{`
                 .listing_box{
@@ -52,6 +53,9 @@ export default function Listing(){
                 }
                 .box_aligner{
                     align-items:center;
+                }
+                .cards_delimiter{
+                    padding:10px;
                 }
             `}</style>
         </>
